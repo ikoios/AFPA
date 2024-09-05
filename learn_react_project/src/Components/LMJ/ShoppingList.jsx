ca5a2b902d3b753d70ced2678ea2fd0c849cdd81
unknown <sr.sullivan.richard@gmail.com>
2024-09-05 15:32:41 +0200

    categories works

--- learn_react_project/src/Components/LMJ/ShoppingList.jsx
+++ learn_react_project/src/Components/LMJ/ShoppingList.jsx
@@ -1,21 +1,31 @@
 import { plantList } from "../../Datas/PlantList";
 import PlantItem from "./PlantItem";
+import { Container, Form } from "react-bootstrap";
+import "../Css/shoppingList.css";
+import { useState } from "react";
 
 export default function ShoppingList({ cart, updateCart }) {
+  const [activeCategory, setActiveCategory] = useState('');
   const categories = plantList.reduce(
     (acc, plante) =>
       acc.includes(plante.category) ? acc : acc.concat(plante.category),
     []
   );
+
   return (
     <div>
-      <ul>
+    <Container className="d-flex justify-content-center">
+      <Form.Select value={activeCategory} onChange={(e) => setActiveCategory(e.target.value)} className="w-25 text-center">
+      <option value={''}>Catégories</option>
         {categories.map((cat) => (
-          <li key={cat}>{cat}</li>
+          <option key={cat} value={cat}>{cat}</option>
         ))}
-      </ul>
+      </Form.Select>
+    </Container>
+
       <ul className="d-flex flex-wrap p-5 justify-content-end">
         {plantList.map((plante) => (
+          !activeCategory || activeCategory === plante.category ? (
           <>
             <PlantItem
             cart={cart}
@@ -23,7 +33,7 @@ export default function ShoppingList({ cart, updateCart }) {
             props={plante}
             />
           </>
-        ))}
+        ): null))}
       </ul>
     </div>
   );
